0c7c6998a1118dd0753aa1448e87d39e711903ef
William Erthal <83590841+williamerthal@users.noreply.github.com>
2021-10-27 12:40:27 -0300

    altera 02

---
+++ src/if-else.js
@@ -0,0 +1,29 @@
+let x = 10;
+
+let condicao = x > 0;
+
+if (condicao) {
+  console.log("X é maior do que zero");
+} else {
+  console.log("X é menor ou igual a que zero");
+}
+
+let estaSol = true;
+if (estaSol) {
+  console.log("Vou pra praia!");
+} else {
+  console.log("Vou ficar em casa!");
+}
+
+let numero = 1;
+if (numero > 0) {
+  console.log("Número positivo");
+} else if (numero == 0) {
+  console.log("Número é zero");
+} else {
+  console.log("Número negativo");
+}
+
+let numero2 = 3;
+let paridade = numero2 % 2 == 0 ? "par" : "ímpar";
+console.log(paridade);
