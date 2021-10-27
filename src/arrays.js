5f12c82f10ee1ba009228be8031cb04a633a1138
William Erthal <83590841+williamerthal@users.noreply.github.com>
2021-10-27 10:32:24 -0300

    Update arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -1,20 +1,32 @@
-import "./styles.css";
-import createList, { alunos } from "./arrays";
+console.debug("arrays.js started.");
 
-console.debug("index.js started.");
+const alunosInterno = ["luizinho", "uguinho", "zezinho"];
+export { alunosInterno as alunos };
 
-var app = document.getElementById("app");
-app.innerHTML = `
-<h3>Hello Vanilla!</h3>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-
-`;
-
-if (!alunos.includes("fulaninho")) {
-  alunos.push("fulaninho");
+export default function createList(pref = "sr") {
+  return (
+    "<ul>" +
+    alunosInterno
+      .map((umAlunoOriginal) => umAlunoOriginal.toUpperCase()) //["LUIZINHO", "UGUINHO", "ZEZINHO", "FULANINHO"]
+      .filter((umAluno) => !umAluno.startsWith("Z")) //["LUIZINHO", "UGUINHO","FULANINHO"]
+      .map((umAlunoFiltrado) => `<li>${pref} ${umAlunoFiltrado}</li>`) //["<li>LUIZINHO</li>", "<li>UGUINHO</li>","<li>FULANINHO</li>"]
+      .reduce((acc, umAlunoLi) => {
+        return acc.concat(umAlunoLi);
+      }) + // <li>LUIZINHO</li><li>UGUINHO</li><li>FULANINHO</li>
+    "</ul>"
+  );
 }
-document.getElementsByClassName("arraysExample")[0].innerHTML = createList();
+
+//Arrays
+let numbers = [1, 2, 3, 4, 5];
+//get first and second
+var [one, two] = numbers; // one = 1, two = 2
+// third is ignored
+var [one, two, , four] = numbers; // one = 1, two = 2, four = 4
+//rest
+var [one, ...rest] = numbers; // one = 1 , rest = [2,3,4,5]
+
+//append
+let numbers2 = [...numbers, 6];
+console.log("numbers=" + numbers);
+console.log("numbers2=" + numbers2);
